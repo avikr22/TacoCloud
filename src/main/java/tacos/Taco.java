0546f8aebd8750afc6e85abc079acb41ae96fab1
avikr22 <avi.kumar.2204@gmail.com>
2019-06-19 15:56:32 +0100

    Developing a Tacocloud implementation

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,48 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Taco {
+	
+	private Long id;
+	private Date createdAt;
+	
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
+	private String name;
+	
+	@Size(min=1, message="You must choose at least 1 ingredient")
+	private List<String> ingredients;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+
+}
